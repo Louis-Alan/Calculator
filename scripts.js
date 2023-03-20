30df6a65bc49891d8ce048b9c4fac4a79c556e77
Alan Louis <alouis811@gmail.com>
2023-03-20 16:43:12 +0530

    created button fn

--- scripts.js
+++ scripts.js
@@ -1,7 +1,49 @@
 console.log("Pepega")
+let displayValue = 0
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+but = document.querySelectorAll('button')
 
-let disp = 7
 
-disp = document.getElementById("display")
+but.forEach(button => {
+    button.addEventListener('click', function() {
+        if(button.className == 'operator'){
+            console.log("hihii")
+        }
+        else if(button.className == 'operand'){
+            console.log("operand")
+        }
+        else if(button.className == 'clear'){
+            console.log("clear")
+        }
+        else if(button.className == 'sign'){
+            console.log("sign")
+        }
+        else if(button.className == 'zero'){
+            console.log("zero")
+        }
+        else if(button.className == 'decimals'){
+            console.log("decimals")
+        }
+        else{
+            console.log("equals")
+        }
+    });
+  });
 
-console.log()
+function updateDisp(){
+    const display = document.getElementById('display');
+    display.innerText = displayValue;
+    if(displayValue.length > 9) {
+        display.innerText = displayValue.substring(0, 9);
+    }
+}
+
+updateDisp()
+
+function oper(op){
+
+}
